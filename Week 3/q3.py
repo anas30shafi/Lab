21e6f0820b500d6f923119b478af3be5855ed8ba
anas30shafi <102944727+anas30shafi@users.noreply.github.com>
2024-08-30 23:22:35 +0530

    Add files via upload

---
+++ Week 3/q3.py
@@ -0,0 +1,3 @@
+my_string = "Hello, World!"
+for i in range(0, len(my_string), 2):
+    print(my_string[i], end=" ")
---
+++ Week 3/q5.py
@@ -0,0 +1,5 @@
+def remove_chars(input_string, n):
+    return input_string[:n]
+
+result = remove_chars("aligarh", 3)
+print(result)  # Output: "al"
